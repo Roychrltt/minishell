05a0b7b5f36757de4ffe06a8687e886f6ea8f54c
Charlotte Roy <roychrltt@chacha.home>
2024-08-18 14:30:56 +0200

    from home

--- src/main.c
+++ src/main.c
@@ -6,14 +6,51 @@
 /*   By: xiaxu <xiaxu@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/07 12:40:25 by xiaxu             #+#    #+#             */
-/*   Updated: 2024/08/07 12:45:16 by xiaxu            ###   ########.fr       */
+/*   Updated: 2024/08/18 14:30:47 by xiaxu            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <stdio.h>
+#include <../includes/minishell.h>
 
-int	main()
+// void rl_clear_history(void);
+// int rl_on_new_line(void);
+// int rl_replace_line(const char *text, int clear_undo);
+// void rl_redisplay(void);
+// void add_history(const char *line)
+//
+
+void	sigint_handler(int signum) {
+	(void)signum; // Unused parameter
+	printf("\n");
+	rl_on_new_line();
+	rl_replace_line("", 0);
+	rl_redisplay();
+}
+
+void sigquit_handler(int signum) {
+	(void)signum;
+	// Ignore SIGQUIT
+	printf("\b\b  \b\b");
+	rl_on_new_line();
+	rl_redisplay();
+}
+
+int	main(int argc, char **argv)
 {
-	printf("Hello, world!\n");
+	char	*input;
+
+	// Set up signal handler for SIGINT
+	signal(SIGINT, sigint_handler);
+	signal(SIGQUIT, sigquit_handler);
+	while (1)
+	{
+		input = readline("minishell");
+		if (!input || !ft_strcmp(input, "exit"))
+			break;
+		if (*input)
+			add_history();
+		free(input);
+	}
+	rl_clear_history();
 	return (0);
 }
