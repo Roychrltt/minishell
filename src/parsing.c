1a5d82c5b26c884a74c1a66cddcc1e72dcb7dcef
Xiaoyun Xu <xiaxu@made-f0Ar12s7.clusters.42paris.fr>
2024-08-20 16:42:07 +0200

    parsing

--- src/parsing.c
+++ src/parsing.c
@@ -6,7 +6,7 @@
 /*   By: xiaxu <xiaxu@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/15 12:24:34 by xiaxu             #+#    #+#             */
-/*   Updated: 2024/08/20 14:56:41 by xiaxu            ###   ########.fr       */
+/*   Updated: 2024/08/20 16:41:43 by xiaxu            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -35,15 +35,13 @@ static void	print_tokens(char **tokens)
 	free_tokens(tokens);
 }
 
-int main(int argc, char **argv)
+int main(void)
 {
-	if (argc != 2)
-		printf("Wrong number of argument!\n");
-
 	char **tokens;
-	if ((ft_strchr(argv[1], '"') == ft_strlen(argv[1])) && (ft_strchr(argv[1], 39) == ft_strlen(argv[1])))
+	char *input = readline("test$>");
+	if ((ft_strchr(input, '"') == ft_strlen(input)) && (ft_strchr(input, 39) == ft_strlen(input)))
 	{
-		tokens = ft_split(argv[1], ' ');
+		tokens = ft_split(input, ' ');
 		print_tokens(tokens);
 		return (0);
 	}
@@ -54,59 +52,78 @@ int main(int argc, char **argv)
 	int status = 0;
 
 	i = 0;
-	while (argv[1][i])
+	while (input[i])
 	{
-		s = ft_strchr(argv[1] + i, 39);
-		d = ft_strchr(argv[1] + i, '"');
-		if (status == 0 || s < d)
+		s = ft_strchr(input + i, '\'');
+		d = ft_strchr(input + i, '"');
+		if (status == 0 && s < d)
 		{
 			status = 1;
-			tokens = ft_split(ft_substr(argv[1], i, s), ' ');
+			tokens = ft_split(ft_substr(input, i, s), ' ');
 			print_tokens(tokens);
 			i += s;
 		}
-		else if (status == 0 || d < s)
+		else if (status == 0 && d < s)
 		{
 			status = 2;
-			tokens = ft_split(ft_substr(argv[1], i, d), ' ');
+			tokens = ft_split(ft_substr(input, i, d), ' ');
 			print_tokens(tokens);
 			i += d;
 		}
 		else if (status == 1)
 		{
+			printf(" ");
 			while (s--)
 			{
-				write (1, &argv[1][i], 1);
+				printf("%c", input[i]);
 				i++;
 			}
+			status = 0;
 		}
 		else if (status == 2)
 		{
+			printf(" ");
 			while (d)
 			{
-				while (d-- && argv[1][i] != '$')
+				if (input[i] != '$')
 				{
-					write (1, &argv[1][i], 1);
+					printf("%c", input[i]);
 					i++;
+					d--;
 				}
-				if (d > 0)
+				else
 				{
-					printf("set_value\n");
-					while (argv[1][i] >= 'A' && argv[1][i] <= 'Z')
-					{
-						i++;
-						d--;
-					}
+					i++;
+					int x;
+					int x1;
+					int x2;
+					x1 = ft_strchr(input + i, ' ');
+					x2 = ft_strchr(input + i, '"');
+					if (x1 <= x2)
+						x = x1;
+					else
+						x = x2;
+					char *to_check = ft_substr(input, i, x);
+					if (!ft_strcmp(to_check, "VALUE") || !ft_strcmp(to_check, "TEST"))
+						status = 3;
+					i += x;
+					d -= x;
+					if (status == 3)
+						printf("set_value");
+					d--;
 				}
+
 			}
+			status = 0;
 		}
 		else
 		{
-			tokens = ft_split(ft_substr(argv[1], i, ft_strlen(argv[1]) - i), ' ');
+			tokens = ft_split(ft_substr(input, i, ft_strlen(input) - i), ' ');
 			print_tokens(tokens);
 			break;
 		}
 		i++;
 	}
+	printf("\n");
 	return 0;
 }
